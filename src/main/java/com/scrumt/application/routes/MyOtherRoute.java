b57386e3fbec9c414fe7b504d8b85307a46e054e
chrismmilburn <chrismmilburn@netscape.net>
2020-12-03 16:36:02 +0000

    Choice testing.

--- src/main/java/com/scrumt/application/routes/MyOtherRoute.java
+++ src/main/java/com/scrumt/application/routes/MyOtherRoute.java
@@ -8,6 +8,18 @@ public class MyOtherRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("file:C://inputFolder2?noop=true").to("file:C://outputFolder");
+        from("file:C://inputFolder2?noop=true")
+            .choice()
+                .when( header("someConstant" ).isGreaterThan( 0) )
+                    .log("found some constant")
+                .endChoice()
+                .when( header("someConstant" ).isGreaterThan( 0) )
+                .log("found some constant")
+                .endChoice()
+                .otherwise()
+                    .log("found NO constant")
+                .endChoice()
+            .end()
+        .to("file:C://outputFolder");
     }
 }
